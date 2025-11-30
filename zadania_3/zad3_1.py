ad1636ac2c51916517a004884e946054e56dfc0c
qash <kuzielamaciej12@gmail.com>
2025-11-30 14:17:18 +0100

    Dodano pliki z zadaniami OOP

---
+++ zadania_3/zad3_1.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    def is_passed(self):
+        return sum(self.marks)/len(self.marks) > 50
+
+student1 = Student("Tomasz", [100,20,34])
+student2 = Student("Konrad", [10,20,30])
+print(student1.is_passed())
+print(student2.is_passed())
