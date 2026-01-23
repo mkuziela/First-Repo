94bb1d64e92dbbd0e04d2a5a776ade85532ab61b
qash <kuzielamaciej12@gmail.com>
2026-01-23 19:32:33 +0100

    Fix

--- project_ai/app/main.py
+++ project_ai/app/main.py
@@ -9,7 +9,6 @@ from app.tasks import detect_people_on_image
 app = FastAPI(title="System Liczenia Osób")
 
 
-# OCENA 3.0 - Synchronicznie
 @app.get("/local")
 def count_local(path: str):
     if not os.path.exists(path):
@@ -18,29 +17,25 @@ def count_local(path: str):
     return {"people_count": count}
 
 
-# OCENA 4.0 - Asynchronicznie (URL)
 @app.get("/process-url")
 def process_url(url: str):
     task = process_image_task.delay("url", url)
     return {"task_id": task.id, "status": "queued"}
 
 
-# OCENA 5.0 - Asynchronicznie (Upload)
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
-    # Zapisz plik
+
     filename = f"{uuid.uuid4()}_{file.filename}"
     file_path = os.path.join("uploaded_images", filename)
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # Kolejkuj zadanie
     task = process_image_task.delay("file", file_path)
     return {"task_id": task.id, "status": "queued"}
 
 
-# Sprawdzanie wyniku
 @app.get("/status/{task_id}")
 def get_status(task_id: str):
     result = AsyncResult(task_id)
